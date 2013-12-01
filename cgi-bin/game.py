2f112b257764cc956d406312e9d6a66ea651dbca
michaelchum <michaelhochum@gmail.com>
2013-12-01 12:12:23 -0500

    Working game

--- cgi-bin/game.py
+++ cgi-bin/game.py
@@ -6,7 +6,7 @@ cgitb.enable()  # for debugging
 
 # Initialize game table
 initGame = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-finalTable = [1,6,3,2,6,5,8,7,2,4,1,8,3,7,4,5]
+solTable = [1,6,3,2,6,5,0,7,2,4,1,0,3,7,4,5]
 
 # Helper function to parse cookie string into int list
 def decodeList(string):
@@ -16,33 +16,45 @@ def decodeList(string):
 	return string_list
 
 # Logout function to delete cookie
-def logout:
-	cookie = Cookie.SimpleCookie(os.environ["HTTP_COOKIE"])
-    cookie['clickCount']['expires'] = 'Thu, 01 Jan 1970 00:00:00 GMT'
-
+def logout():
+	try:
+		cookie = Cookie.SimpleCookie(os.environ["HTTP_COOKIE"])
+		cookie['clickCount']['expires'] = 'Thu, 01 Jan 1970 00:00:00 GMT'
+		cookie['userTable']['expires'] = 'Thu, 01 Jan 1970 00:00:00 GMT'
+		print cookie # IMPORTANT SAVE cookie to expire immediately
+	except (Cookie.CookieError, KeyError):
+		return
 
 # Check if cookie exists if not, initialize cookie
 try:
-    cookie = Cookie.SimpleCookie(os.environ["HTTP_COOKIE"])
-    userTable=cookie['userTable'].value
+	cookie = Cookie.SimpleCookie(os.environ["HTTP_COOKIE"])
+	# Retrieve cookie info
+	clickCount = int(cookie['clickCount'].value)
+	userTable = decodeList(cookie['userTable'].value)
 except (Cookie.CookieError, KeyError):
 	cookie = Cookie.SimpleCookie()
-	cookie['clickCount']=0
-	cookie['userTable']=initGame
+	cookie['clickCount'] = 0
+	cookie['userTable'] = initGame
 	cookie['clickCount']['expires'] = 1*1*3*60*60 # Expires in 3 hours
 	cookie['userTable']['expires'] = 1*1*3*60*60
-	print cookie # IMPORTANT SAVE cookie 
+	print cookie # IMPORTANT SAVE cookie
+	# Retrieve cookie info
+	clickCount = int(cookie['clickCount'].value)
+	userTable = decodeList(cookie['userTable'].value)
 
-# Check get request for card ID
+
+# Check GET/POST request
 if (cgi.FieldStorage()):
 	form = cgi.FieldStorage()
-	card = form.getvalue('card')
-	card = int(card)
-	userTable = cookie['userTable'].value
-	userTable = decodeList(userTable)
-	userTable[card] = 1
-	cookie['userTable'] = userTable
-	print cookie # IMPORTANT SAVE cookie 
+	# Check GET request for card ID
+	if (form.getvalue('card')):
+		card = form.getvalue('card')
+		card = int(card)
+		userTable = cookie['userTable'].value
+		userTable = decodeList(userTable)
+		userTable[card] = 1
+		cookie['userTable'] = userTable
+		print cookie # IMPORTANT SAVE cookie
 
 topBody = '''
 <html>
@@ -59,42 +71,30 @@ botBody = '''
 	</body>
 </html>
 '''
-originalTable = '''
-		<table>
-			<tr>
-				<td><a href=game.py?card=0><img src="/img/back.png"></a></td>
-				<td><a href=game.py?card=1><img src="/img/back.png"></a></td>
-				<td><a href=game.py?card=2><img src="/img/back.png"></a></td>
-				<td><a href=game.py?card=3><img src="/img/back.png"></a></td>
-			</tr>
-			<tr>
-				<td><a href=game.py?card=4><img src="/img/back.png"></a></td>
-				<td><a href=game.py?card=5><img src="/img/back.png"></a></td>
-				<td><a href=game.py?card=6><img src="/img/back.png"></a></td>
-				<td><a href=game.py?card=7><img src="/img/back.png"></a></td>
-			</tr>
-			<tr>
-				<td><a href=game.py?card=8><img src="/img/back.png"></a></td>
-				<td><a href=game.py?card=9><img src="/img/back.png"></a></td>
-				<td><a href=game.py?card=10><img src="/img/back.png"></a></td>
-				<td><a href=game.py?card=11><img src="/img/back.png"></a></td>
-			</tr>
-			<tr>
-				<td><a href=game.py?card=12><img src="/img/back.png"></a></td>
-				<td><a href=game.py?card=13><img src="/img/back.png"></a></td>
-				<td><a href=game.py?card=14><img src="/img/back.png"></a></td>
-				<td><a href=game.py?card=15><img src="/img/back.png"></a></td>
-			</tr>
-		</table>
-'''
+
+def printGameTable():
+	print '<table>'
+	print '<tr>'
+	for x in range(16):
+		if (userTable[x]==0):
+			print '<td><a href=game.py?card=' + str(x) + '><img src="/img/back.png"></a></td>'
+		elif (userTable[x]==9):
+			print '<td><img src="/img/blank.jpg"></td>'
+		elif (userTable[x]==1):
+			print '<td><img src="/img/card' + str(solTable[x]) + '.jpg"></td>'
+		if (x==3 or x==7 or x==11):
+			print '</tr>'
+			print '<tr>'
+	print '</tr>'
+	print '</table>'
 
 metaRefresh = '<meta http-equiv="refresh" content="2;url=game.py">'
-
 print 'Content-type: text/html\r\n\r'
-#print topBody
-print cookie['userTable'].value
+print topBody
+printGameTable()
+print botBody
 #print originalTable
 #print metaRefresh
-#print botBody
+
 
 
